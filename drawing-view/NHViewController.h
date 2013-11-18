ee47cc2be2a0e23596e54bd426b104ed207815f0
ryan johnson <ryan@mobiledatasolutions.net>
2013-11-18 16:43:03 -0600

    Initial Commit

---
+++ drawing-view/NHViewController.h
@@ -0,0 +1,13 @@
+//
+//  NHViewController.h
+//  drawing-view
+//
+//  Created by Ryan Johnson on 11/18/13.
+//  Copyright (c) 2013 NHCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NHViewController : UIViewController
+
+@end
---
+++ drawing-view/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  drawing-view
+//
+//  Created by Ryan Johnson on 11/18/13.
+//  Copyright (c) 2013 NHCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NHAppDelegate class]));
+    }
+}
